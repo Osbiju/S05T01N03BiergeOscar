c2d45a92e74c8ad0645579653997b4620515b9d9
Oscar <okeshik@protonmail.com>
2024-03-14 13:18:45 +0100

    Initial commit, estructura

---
+++ src/test/java/cat/itacademy/barcelonactiva/BiergeOscar/s05t01n03/S05T01N03BiergeOscarApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.BiergeOscar.s05t01n03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N03BiergeOscarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
